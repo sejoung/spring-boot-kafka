9b56c993272adf27a3ce55a78e4825a44dbf36f7
sejoung kim <sejoung@gmail.com>
2018-10-08 11:45:11 +0900

    디시리얼라이즈
    디시리얼라이즈

--- src/main/java/com/github/sejoung/Application.java
+++ src/main/java/com/github/sejoung/Application.java
@@ -8,6 +8,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.annotation.PartitionOffset;
 import org.springframework.kafka.annotation.TopicPartition;
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.messaging.handler.annotation.Header;
+import org.springframework.messaging.handler.annotation.Payload;
+
+import com.github.sejoung.domain.TestDto;
 
 /**
  * @author kim se joung
@@ -22,10 +27,22 @@ public class Application {
         SpringApplication.run(Application.class, args);
     }
 
-    @KafkaListener(topicPartitions = { @TopicPartition(topic = "test", partitionOffsets = @PartitionOffset(partition = "0", initialOffset = "0")) })
+    //@KafkaListener(topicPartitions = { @TopicPartition(topic = "test", partitionOffsets = @PartitionOffset(partition = "0", initialOffset = "0")) })
     public void listen(ConsumerRecord<?, ?> cr) throws Exception {
         logger.info( cr.value().toString());
        
+    }
+    
+    @KafkaListener(topicPartitions = { @TopicPartition(topic = "ClickViewData", partitionOffsets = @PartitionOffset(partition = "0", initialOffset = "0")) })
+    public void listen(@Payload TestDto payload,
+            @Header(KafkaHeaders.RECEIVED_MESSAGE_KEY) String key,
+            @Header(KafkaHeaders.RECEIVED_PARTITION_ID) int partition,
+            @Header(KafkaHeaders.RECEIVED_TOPIC) String topic,
+            @Header(KafkaHeaders.RECEIVED_TIMESTAMP) long ts
+            ) {
+        
+            logger.info("RECEIVED_MESSAGE_KEY={}, RECEIVED_PARTITION_ID={}, RECEIVED_TOPIC={}, RECEIVED_TIMESTAMP={}", key, partition, topic, ts);
+            logger.info("Payload={}",payload);
     }
 
 }
---
+++ src/main/java/com/github/sejoung/domain/TestDto.java
@@ -0,0 +1,24 @@
+package com.github.sejoung.domain;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@ToString
+@Setter
+@Getter
+public class TestDto {
+
+    @JsonProperty("NO")
+    private int no;
+    
+    @JsonProperty("PCODE")
+    private String pcode;
+
+    @JsonProperty("adGubun")
+    private String adGubun;
+    
+
+}
